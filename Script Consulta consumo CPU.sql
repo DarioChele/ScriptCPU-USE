1f05ea7af763d1cc51aa881e45df1ea3351e730b
Dario Chele Salinas <dario.chele@hotmail.com>
2023-03-08 17:24:08 -0500

    Update and rename Script Consulta consumo CPU.txt to Script Consulta consumo CPU.sql

--- Script Consulta consumo CPU.sql
+++ Script Consulta consumo CPU.sql
@@ -1,8 +1,9 @@
+
 -- Existe una consulta concreta, a tiempo real, que nos permite ver el consumo de cpu del SQL Server, identificado por hostname de origen.
  
--- Esta consulta se debe ejecutar desde el Query Analyzer (Analizador de Consultas), y nos generar· una tabla con la informaciÛn necesaria.
+-- Esta consulta se debe ejecutar desde el Query Analyzer (Analizador de Consultas), y nos generar√° una tabla con la informaci√≥n necesaria.
  
--- En el Query analyzer, debemos indicar primero a quÈ servidor queremos conectar, y cuando tengamos la nueva consulta abierta; cambiar en el desplegable de -- bases de datos, a la BBDD 'master'.
+-- En el Query analyzer, debemos indicar primero a qu√© servidor queremos conectar, y cuando tengamos la nueva consulta abierta; cambiar en el desplegable de -- bases de datos, a la BBDD 'master'.
  
 -- Dentro del analizador, copiaremos:
  
